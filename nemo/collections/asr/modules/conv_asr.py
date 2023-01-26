c561d98d8df0ee64dc8f02ba0a3d2eaf2eeae1f0
George <zelenfrojnd.gm@phystech.edu>
2023-01-26 20:15:26 +0400

    add logpoinyts
    Signed-off-by: George <zelenfrojnd.gm@phystech.edu>

--- nemo/collections/asr/modules/conv_asr.py
+++ nemo/collections/asr/modules/conv_asr.py
@@ -263,25 +263,32 @@ class ConvASREncoder(NeuralModule, Exportable):
     @typecheck()
     def forward(self, audio_signal, length):
         self.update_max_seq_length(seq_length=audio_signal.size(2), device=audio_signal.device)
-        max_audio_length: int = audio_signal.size(-1)
+        #max_audio_length: int = audio_signal.size(-1)
+        #max_audio_length = audio_signal.size(1)
+
         if isinstance(self.pre_encode, nn.Linear):
             audio_signal = self.pre_encode(audio_signal)
         else:
+            logging.warning(f"FRWD pnt 1")
             audio_signal, length = self.pre_encode(x=audio_signal, lengths=length)
 
-        max_audio_length = audio_signal.size(1)
+        #max_audio_length = audio_signal.size(1)
 
         if self.out_proj is not None:
+            logging.warning(f"FRWD pnt 2")
             audio_signal = self.out_proj(audio_signal)
 
         if self.reduction_position == -1:
+            logging.warning(f"FRWD pnt 3")
             audio_signal, length = self.reduction_subsampling(x=audio_signal, lengths=length)
 
+        logging.warning(f"FRWD pnt 4")
         audio_signal = torch.transpose(audio_signal, 1, 2)
 
+        logging.warning(f"FRWD pnt 5")
         s_input, length_1 = self.encoder(([audio_signal], length))
-        if length_1 is None:
-            pass#return s_input[-1]
+        #if length_1 is None:
+         #   pass#return s_input[-1]
 
         #return s_input[-1], length
         logging.warning(f"PREV DATA s_inp{s_input[-1]}, length {length_1}")
